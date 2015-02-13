58b693ebc3d942ef7e35a3c08d6faf45a6dd2fa4
Chris Henderson <chris@waitingforthefuture.org>
2015-02-12 19:16:54 -0500

    remove unnecessary imports

--- sources/chrome.py
+++ sources/chrome.py
@@ -1,11 +1,9 @@
 import os
-import glob
 import shutil
 import sqlite3
 
 import pytz
 from dateutil import parser
-from django.db import IntegrityError
 from django.conf import settings
 from django.utils.html import escape
 
@@ -20,6 +18,12 @@ class DataSource(HoursDataSource):
             raise BaseException("Chrome history location not specified.")
         start_time = self.start_date.strftime('%s')
         end_time = self.end_date.strftime('%s')
+        copied_file = os.path.join(settings.BASE_DIR, 'data/chrome/History')
+        orig_file = settings.DS['CHROME']['HISTORY']
+        # Copy file if we haven't copied it before, or if it's outdated
+        if not os.path.exists(copied_file) or os.stat(orig_file).st_mtime - os.stat(copied_file).st_mtime > 0:
+            print "Copying history"
+            shutil.copy2(orig_file, copied_file)
         # See http://linuxsleuthing.blogspot.ca/2011/06/decoding-google-chrome-timestamps-in.html
         query = """SELECT DATETIME((visit_time/1000000)-11644473600, 'unixepoch') AS dt,
                 title, urls.url
@@ -27,12 +31,6 @@ class DataSource(HoursDataSource):
                 AND (visit_time/1000000)-11644473600 > %s
                 AND (visit_time/1000000)-11644473600 < %s
                 ORDER BY visit_time ASC;""" % (start_time, end_time)
-        copied_file = os.path.join(settings.BASE_DIR, 'data/chrome/History')
-        orig_file = settings.DS['CHROME']['HISTORY']
-        # Copy file if we haven't copied it before, or if it's outdated
-        if not os.path.exists(copied_file) or os.stat(orig_file).st_mtime - os.stat(copied_file).st_mtime > 0:
-            print "Copying history"
-            shutil.copy2(orig_file, copied_file)
         conn = sqlite3.connect(copied_file)
         cursor = conn.cursor()
         db_results = cursor.execute(query)
